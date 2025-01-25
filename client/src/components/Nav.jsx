6b7df9c31f276d31e04d7f1a28443ce5e8b0fa4e
Angelbyte <roberto.angel96@live.com>
2025-01-25 14:38:37 -0600

    Ajustar el ancho del componente de navegación para que ocupe el 100% del contenedor

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 const Nav = () => {
     return (
-        <nav className="flex items-center justify-center bg-[#17142341] shadow-lg p-1">
+        <nav className="flex items-center justify-center w-full bg-[#17142341] shadow-lg p-1">
             <h1 className='text-4xl text-white py-2 uppercase'>Angelbyte Auth</h1>
         </nav>
     )
