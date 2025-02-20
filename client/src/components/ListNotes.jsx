b13580d0cdbe96d39ac877b92f8248e4283a1883
Angelbyte <roberto.angel96@live.com>
2025-02-20 02:39:21 -0600

    Agregar ordenamiento por fecha de creación en la consulta de notas en el componente ListNotes

--- client/src/components/ListNotes.jsx
+++ client/src/components/ListNotes.jsx
@@ -20,7 +20,7 @@ const ListNotes = ({ refreshTrigger, onNoteAdded }) => {
 			try {
 				setIsLoading(true)
 				const fetchApi = await fetch(
-					`${STRAPI_URL}/api/note?populate=user&filters[user][documentId]=${userId}`,
+					`${STRAPI_URL}/api/note?populate=user&filters[user][documentId]=${userId}&sort=createdAt`,
 					{
 						headers: {
 							Authorization: `Bearer ${token}`
