457be579780bad30209a528a5aa222908322a567
Angelbyte <roberto.angel96@live.com>
2025-02-24 19:00:12 -0600

    Refactor el componente AuthCallback para simplificar la lógica de autenticación y mejorar la legibilidad del código.

--- client/src/components/AuthCallback.jsx
+++ client/src/components/AuthCallback.jsx
@@ -1,32 +1,29 @@
-import { useEffect } from 'react';
-import { navigate } from 'astro/virtual-modules/transitions-router.js';
+import { useEffect } from 'react'
+import { navigate } from 'astro/virtual-modules/transitions-router.js'
 
 const AuthCallback = () => {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    console.log(params);
-    
-    // Verifica si Strapi devuelve el token en un parámetro "jwt" (ajusta según lo que observes en la red)
-    const jwt = params.get('id_token');
-    const username = params.get('username'); // Si Strapi incluye más datos, ajusta aquí.
-    const id = params.get('id');
-    console.log(jwt, username, id);
-    
+	useEffect(() => {
+		const params = new URLSearchParams(window.location.search)
 
-    if (jwt) {
-      localStorage.setItem('user', JSON.stringify({ token: jwt, username, id }));
-      navigate('/dashboard');
-    } else {
-      console.error('Faltan parámetros de autenticación en la URL');
-      navigate('/');
-    }
-  }, []);
+		// Verifica si Strapi devuelve el token en un parámetro "jwt" (ajusta según lo que observes en la red)
+		const jwt = params.get('id_token')
+		const username = params.get('username') // Si Strapi incluye más datos, ajusta aquí.
+		const id = params.get('id')
 
-  return (
-    <div>
-      <p>Procesando autenticación...</p>
-    </div>
-  );
-};
+		if (jwt) {
+			localStorage.setItem('user', JSON.stringify({ token: jwt, username, id }))
+			navigate('/dashboard')
+		} else {
+			console.error('Faltan parámetros de autenticación en la URL')
+			navigate('/')
+		}
+	}, [])
 
-export { AuthCallback };
+	return (
+		<div>
+			<p>Procesando autenticación...</p>
+		</div>
+	)
+}
+
+export { AuthCallback }
