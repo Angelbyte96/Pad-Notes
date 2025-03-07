397359ec4ad1b9b82971890534879e13993008a0
Angelbyte <roberto.angel96@live.com>
2025-03-07 17:07:15 -0600

    Incluir 'tslib' como alias en la configuración de Vite y optimizar su inclusión en las dependencias

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -2,15 +2,21 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
 import vercel from '@astrojs/vercel'
+import { resolve } from 'path'
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@/': './src'
+        '@/': './src',
+        // Se resuelve "tslib" a una ruta absoluta
+        'tslib': resolve('./node_modules/tslib/tslib.es6.js')
       },
       dedupe: ['react', 'react-dom']
+    },
+    optimizeDeps: {
+      include: ['tslib']
     },
     build: {
       rollupOptions: {
