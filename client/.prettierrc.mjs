a63c74c3d2db10253d86cf6bfd1ee7430c041119
Angelbyte <roberto.angel96@live.com>
2025-05-02 10:15:54 -0600

    Se agrega config de prettier y Eslint, así como dependencias necesarias

---
+++ client/.prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+export default {
+	printWidth: 100,
+	useTabs: true,
+  // Tabulación con 2 espacios
+	tabWidth: 2,
+	// Punto y coma al final de cada línea
+	semi: false,
+	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+	overrides: [
+		{
+			files: ["**/*.astro"],
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+};
