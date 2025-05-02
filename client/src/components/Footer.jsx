1274fa254fa84f837577035d22ed01adbcd770d7
Angelbyte <roberto.angel96@live.com>
2025-05-02 16:15:56 -0600

    Ajusta el espaciado en el componente Footer para mejorar la presentación

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Heart } from 'lucide-react'
 
 const Footer = () => {
 	return (
-		<footer className="mx-auto flex w-full items-center justify-center border-t px-4 py-2 pb-1 font-semibold md:py-4">
+		<footer className="mx-auto flex w-full items-center justify-center border-t px-4 py-2 font-semibold md:py-2">
 			<p className="flex w-full justify-start gap-1">
 				Hecho con{' '}
 				<span>
