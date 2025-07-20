ea66aa47bff6873c524f794c24e35faa293ce701
Angelbyte96 <108975993+Angelbyte96@users.noreply.github.com>
2025-07-20 12:50:29 +0000

    Ajusta el tamaño de fuente del título en el componente Nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
 	return (
 		<nav className="flex sticky top-0 z-50 w-full items-center justify-between bg-[#ffffff] dark:bg-neutral-900 p-0.5 px-4 shadow-lg">
 			<div className="w-6"></div>
-			<h1 className="justify-self-center py-1 text-xl font-semibold text-black uppercase md:text-4xl dark:text-white">
+			<h1 className="justify-self-center py-1 text-xl font-semibold text-black uppercase md:text-2xl dark:text-white">
 				pad notes
 			</h1>
 			<ThemeToggle />
